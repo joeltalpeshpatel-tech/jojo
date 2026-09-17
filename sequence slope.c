81f66fd56edb012bdfe3aa3e6296f26aa8e5145c
joeltalpeshpatel-tech <joeltalpeshpatel@gmail.com>
2026-09-17 09:25:07 +0530

    Add files via upload

---
+++ sequence slope.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+      int a,b,j;
+      scanf("%d", &b);
+      for (a=1;a<=b;a++)
+      {
+      for (j=1;j<=a;j++)
+      {
+      printf("%d", j);
+      }
+      printf("\n");
+      }
+
+      }
